ae3a3d88911b9e2721adc74fc5b4615acf0350a3
andrum99 <58046090+andrum99@users.noreply.github.com>
2020-03-08 10:48:32 +0000

    Create TEMPLATE.sh
    Create a template file to document the required format of test scripts, including warning about shell compatibility. The intent is to assist third party developers who may wish to develop their own tests which slot into Agnostics.

---
+++ data/TEMPLATE.sh
@@ -0,0 +1,18 @@
+# Raspberry Pi agnostics utility - template file for test definition
+# Shebang lines are ignored - invoke using e.g. 'sh mytest.sh'.
+
+# *** N.B. - shell compatibility ***
+# Agnostics calls /bin/sh, which is the dash shell on Debian Buster -
+# beware of bashisms if you use bash to develop your test script.
+# See https://wiki.archlinux.org/index.php/Dash for background info on dash.
+
+# NAME - name of test as it should appear in the GUI
+#NAME=SD Card Speed Test
+
+# DESC - Brief description of what the test does
+#DESC=Determines whether an SD card can read and write data fast enough to provide adequate performance.\n\nShould be run on a new or newly-formatted SD card.
+
+# Put code that performs the test here.
+
+# Script must return 0 if test passed, 1 if test failed.
+# return $pass
